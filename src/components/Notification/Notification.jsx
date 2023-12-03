dfab44fde706fd8c5fbf86618b4fc00cb2009c9c
KovalUser19 <134967853+KovalUser19@users.noreply.github.com>
2023-12-03 20:41:49 +0200

    add

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,5 +1,5 @@
 import css from './Notification.module.css'
-export const Notification = (style) => {
+export const Notification = () => {
   return (
     <div className={css.notification}>
       <p>There is no feedback</p>
